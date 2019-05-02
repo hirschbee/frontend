f0c2e4fc289e04503c171c5d4d2ef8fa5ab88976
hirschbee <48605067+hirschbee@users.noreply.github.com>
2019-05-02 10:09:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=320, initial-scale=1">
+    <meta charset="utf-8">
+    
+    <link rel="stylesheet" href="index.css"/>
+    <script defer src="script.js"></script>
+  </head>
+  <body>
+    <div id="header">
+      <h1 id="title">hirschbee</h1>
+       <h3 id="subtitle">front-end development</h3>
+    </div>
+    <div id="main">
+      <p id="par">content inside</p>
+      <p><a href="form.html">guestbook</a></p>
+    </div>
+    <div id="footer">
+      <p>hirschbee<span>&copy;</span>front-end</p>
+    </div>
+    
+  </body>
+</html>
--- server.php
+++ server.php
@@ -1,14 +1,38 @@
 <?php
-$servername = "files.000webhost.com";
-$username = "hirschbaeck";
-$password = "obupno44";
+$servername = "sql201.epizy.com";
+$username = "epiz_23847122";
+$password = "7kG7te4t0";
+$db = "epiz_23847122_guestbook";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+echo $fName=$_POST["fName"];
+echo $lName=$_POST["lName"];
+echo $age=$_POST["age"];
 
+
+$conn = mysqli_connect($servername, $username, $password);
+
+ 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+$conn = new mysqli($servername, $username, $password, $db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+
+$sql = "INSERT INTO guests(fName, lName, age)
+VALUES ('$fName', '$lName', '$age')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 ?>
